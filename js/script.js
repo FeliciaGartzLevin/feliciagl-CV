bb412ae7992caa49b52a8ff198831519ed29417b
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-02-16 15:53:11 +0100

    taking back navbar for small screens

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-// hamburger.addEventListener("click", () =>{
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// })
+
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
 
 function reveal() {
